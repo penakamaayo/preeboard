a18403b6ef974dd0b2fb385fcca897fcec4a79d5
penya <penakamaayo@gmail.com>
2014-09-03 08:58:13 +0800

    added extra fields on user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,40 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
+
+  protected
+  
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) do |u|
+  		u.permit(:first_name, :last_name, :course, :school, :email, :password, :password_confirmation)
+  	end
+  	devise_parameter_sanitizer.for(:account_update) do |u|
+  		u.permit(:first_name, :last_name, :course, :school, :email, :username, :password, :password_confirmation, :current_password, :profile_photo)
+  	end
+  end
+
+  def configure_devise_permitted_parameters
+  	registration_params = [:first_name, :last_name, :course, :school, :email, :username, :password, :password_confirmation]
+
+  	if params[:action] == 'update'
+  		devise_parameter_sanitizer.for(:account_update) { 
+  			|u |u.permit(:first_name, :last_name, :short_bio, :email, :username, :password, :password_confirmation, :current_password)
+  			# |u| u.permit(registration_params << :current_password)
+  		}
+  	elsif params[:action] == 'create'
+  		devise_parameter_sanitizer.for(:sign_up) { 
+  			|u| u.permit(registration_params) 
+  		}
+  	end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+  	new_user_session_path
+  end
+
 end
--- db/migrate/20140902052821_add_extra_fields_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddExtraFieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :course, :string
-    add_column :users, :school, :string
-    add_column :users, :birth_date, :date
-  end
-end
