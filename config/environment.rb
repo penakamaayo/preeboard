58946a917d032606ce2c61929d5688f2cd2973a9
penya <penakamaayo@gmail.com>
2014-09-07 00:21:13 +0800

    added msmtp mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = { 
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "peter.go.sabalai",
+  :user_name            => "dummy.james2014@gmail.com",
+  :password             => "testing 123",
+  :authentication       => "plain",
+  :enable_starttls_auto => true,
+}
